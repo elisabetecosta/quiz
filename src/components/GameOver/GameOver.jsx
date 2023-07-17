fd98c2c668f58c8f95a4aea30adb91b1fc6707b0
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-07-17 15:14:19 +0100

    added info to the GameOver component and logic needed to start a new game

--- src/components/GameOver/GameOver.jsx
+++ src/components/GameOver/GameOver.jsx
@@ -5,13 +5,15 @@ import styles from "./styles.module.css"
 
 function GameOver() {
 
+    const [quizState, dispatch] = useContext(QuizContext)
+
     return (
         <div className={styles.gameOver}>
-            <h2>Game Over</h2>
-            <p>Score: x</p>
-            <p>You got y out of z questions right.</p>
+            <h2>You reached the end of this quiz!</h2>
+            <p>Score: {quizState.score}</p>
+            <p>You got {quizState.score} out of {quizState.questions.length} questions right.</p>
             <img src={WellDone} alt="End of the Quiz" />
-            <button>Play Again</button>
+            <button onClick={() => dispatch({ type: "NEW_GAME" })}>Play Again</button>
         </div>
     )
 }
